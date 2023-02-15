2be5338027609daffcb3da2b4ad935cf04a9f3ca
DROPLOTHUB <72875833+DROPLOTHUB@users.noreply.github.com>
2023-02-15 23:23:35 +0000

    Add files via upload

---
+++ Captive-Portal.ino
@@ -0,0 +1,41 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "NodeMCU"; // Change this to your desired network name
+const char* password = "password"; // Change this to your desired network password
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.sendHeader("Location", "/login");
+  server.send(302, "text/plain", "");
+}
+
+void handleLogin() {
+  if (server.hasArg("username") && server.hasArg("password")) {
+    String enteredUsername = server.arg("username");
+    String enteredPassword = server.arg("password");
+    Serial.print("Entered username: ");
+    Serial.println(enteredUsername);
+    Serial.print("Entered password: ");
+    Serial.println(enteredPassword);
+  }
+  String html = "<!DOCTYPE html><html><head><title>Login Page</title><style>body{background-color:#f2f2f2;font-family:Arial,Helvetica,sans-serif;}form{border:3px solid #f1f1f1;margin:8% 25%;padding:20px;background-color:white;}input[type=text],input[type=password]{width:100%;padding:12px 20px;margin:8px 0;display:inline-block;border:1px solid #ccc;box-sizing:border-box;}button{background-color:#4CAF50;color:white;padding:14px 20px;margin:8px 0;border:none;cursor:pointer;width:100%;}</style></head><body><form method='post' action='/login'><h2>Login to Access the Internet</h2><label for='username'><b>Username</b></label><input type='text' placeholder='Enter Username' name='username' required><label for='password'><b>Password</b></label><input type='password' placeholder='Enter Password' name='password' required><button type='submit'>Login</button></form></body></html>";
+  server.send(200, "text/html", html);
+}
+
+void setup() {
+  WiFi.softAP(ssid, password); // Set up access point
+  IPAddress IP = WiFi.softAPIP(); // Get IP address of the access point
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  server.on("/", handleRoot);
+  server.on("/login", handleLogin);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
